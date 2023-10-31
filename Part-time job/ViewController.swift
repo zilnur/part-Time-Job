82b1a25d76756f0a2869935de05f39acf1ceb703
zilnur <86298777+zilnur@users.noreply.github.com>
2023-10-31 07:52:15 +0300

    Initial Commit

---
+++ Part-time job/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Part-time job
+//
+//  Created by Ильнур Закиров on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
